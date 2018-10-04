8b9aa1a3d902ec98b31d7c102a30adbf6746e375
RednibCoding <binder.mich@gmx.de>
2018-10-04 18:36:44 +0200

    Popup when saving added

--- config.py
+++ config.py
@@ -43,6 +43,31 @@ class Config(ConfigParser):
 		self._cronTurnOn=self.get("Cron", "Turn_On")
 		self._cronTurnOff=self.get("Cron", "Turn_Off")
 
+	def update(self):
+		# Network
+		self.set("Network", "Rpi_Ip", self._rpiIp)
+		self.set("Network", "Rpi_Port", self._rpiPort)
+		self.set("Network", "Remote_Code", self._remoteCode)
+		# Control
+		self.set("Layout", "btn1", self._btnTexts[1])
+		self.set("Layout", "btn2", self._btnTexts[2])
+		self.set("Layout", "btn3", self._btnTexts[3])
+		self.set("Layout", "btn4", self._btnTexts[4])
+		self.set("Layout", "btn5", self._btnTexts[5])
+		self.set("Layout", "btn6", self._btnTexts[6])
+		self.set("Layout", "btnOff", self._btnTexts[0])
+		# Switch Codes
+		self.set("Switch_Codes", "btn1", self._switchCodes[0])
+		self.set("Switch_Codes", "btn2", self._switchCodes[1])
+		self.set("Switch_Codes", "btn3", self._switchCodes[2])
+		self.set("Switch_Codes", "btn4", self._switchCodes[3])
+		self.set("Switch_Codes", "btn5", self._switchCodes[4])
+		self.set("Switch_Codes", "btn6", self._switchCodes[5])
+		# Cron
+		self.set("Cron", "Enabled", self._cronEnabled)
+		self.set("Cron", "Turn_On", self._cronTurnOn)
+		self.set("Cron", "Turn_Off", self._cronTurnOff)
+
 	def saveFile(self, cfgFile):
 		with open(cfgFile, "w") as configfile:
-			self.write(configfile)
+			self.write()
