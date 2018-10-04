3f15c33864b6cafff4649a3d54b237cc3b64292a
RednibCoding <binder.mich@gmx.de>
2018-10-04 14:41:47 +0200

    Added android debug script

---
+++ dbga.py
@@ -0,0 +1,10 @@
+
+# use this script to debug your android device while running the app
+# Developer options and USB-debugging have to be enabled on the device
+# also adb has to be installed on the dev machine (ubuntu: sudo apt-get install adb)
+#
+# you can edit grep tags to change the output (currently, only python errors
+# and it's occurence will be displayed)
+
+import os
+os.system("adb shell logcat | grep -E 'Traceback|File '")
