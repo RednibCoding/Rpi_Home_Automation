697ec1445531b1490cbf6642f1a2b2df5f90ee11
Manuelraa <manuel@bloodycrystals.de>
2018-10-09 20:44:24 +0200

    Refactoring code.

--- rpi_server/rpi_server.py
+++ rpi_server/rpi_server.py
@@ -1,101 +1,110 @@
 #!/usr/bin/env python3
 
 """
-	Rpi Server
-	
-author:		Michael Binder
-dependencies:	tcp.py, RPi.GPIO, sys, ConfigParser
-description:	Establishes a connection via Tcp/Ip in the local network 
-		and waits for messages sent from the app.
-		Then it evaluates those messages and sends them via the connected 433MHz RF-Module
-		to the 433MHz receivers (eg. 433MHz outlets)
+    Rpi Server
+
+author:     Michael Binder
+dependencies:   tcp.py, RPi.GPIO, sys, ConfigParser
+description:    Establishes a connection via Tcp/Ip in the local network
+        and waits for messages sent from the app.
+        Then it evaluates those messages and sends them via the connected 433MHz RF-Module
+        to the 433MHz receivers (eg. 433MHz outlets)
 """
 
-from tcp import Tcp
+
+from configparser import ConfigParser
+from logging import (
+    Logger,
+    StreamHandler
+)
 import RPi.GPIO as gpio
-import sys
-from ConfigParser import ConfigParser
 
-myTcp=Tcp(port=5000, bufferLen=1024, ip="0.0.0.0")
+from tcp import Tcp
 
-gpio.setwarnings(False)
-gpio.setmode(gpio.BCM)
-oPins=[None]*4
-oPins[0]=27
 
-gpio.setup(oPins[0], gpio.OUT)
-
-for i in range(len(oPins)):
-	if oPins[i]:
-		gpio.setup(oPins[i], gpio.OUT)
-
-myTcp.bindSocket("0.0.0.0")
+OPINS = [27]
 
 
 def isOutputPinHigh(pin):
-    return gpio.input(pin)
-
-    
-cfg=ConfigParser()
-cfg.read("config.ini")
-cronEnabled=cfg.getboolean("Cron", "enabled")
-cronTurnOn=cfg.get("Cron", "turn_on")
-cronTurnOff=cfg.get("Cron", "turn_off")
-btn1cronOnOff=cfg.get("Cron", "btn1crononoff")
-btn2cronOnOff=cfg.get("Cron", "btn2crononoff")
-btn3cronOnOff=cfg.get("Cron", "btn3crononoff")
-btn4cronOnOff=cfg.get("Cron", "btn4crononoff")
-btn5cronOnOff=cfg.get("Cron", "btn5crononoff")
-btn6cronOnOff=cfg.get("Cron", "btn6crononoff")
+    if pin:
+        return gpio.input(pin)
+    raise ValueError('Pin not set!')
 
 
-print("Server running:")
-print("")
+class RpiServer:
+    '''Class which manages the IOT server'''
 
-try:
-	while True:
-		msg=myTcp.recvMsg()
-		
-		if "Button 1" in msg:
-			if not isOutputPinHigh(oPins[0]):
-				gpio.output(oPins[0], gpio.HIGH)
-			elif isOutputPinHigh(oPins[0]):
-				gpio.output(oPins[0], gpio.LOW)
-		
-		elif "Button 2" in msg:
-			gpio.output(oPins[0], gpio.LOW)
-			
-		elif "ALL_OFF" in msg:
-			for i in range(len(oPins)):
-				if oPins[i]:
-					if isOutputPinHigh(oPins[i]):
-						gpio.output(oPins[i], gpio.LOW)
-						
-		elif "Cron:" in msg:
-			newMsg=msg.replace("Cron:", "")
-			listMsg=newMsg.split("|")
-			cfg.set("Cron", "enabled", listMsg[0])
-			cfg.set("Cron", "turn_on", listMsg[1])
-			cfg.set("Cron", "turn_off", listMsg[2])
-			cfg.set("Cron", "btn1crononoff", listMsg[3])
-			cfg.set("Cron", "btn2crononoff", listMsg[4])
-			cfg.set("Cron", "btn3crononoff", listMsg[5])
-			cfg.set("Cron", "btn4crononoff", listMsg[6])
-			cfg.set("Cron", "btn5crononoff", listMsg[7])
-			cfg.set("Cron", "btn6crononoff", listMsg[8])
-			with open("config.ini", 'w') as configfile:
-				cfg.write(configfile)
-			
-			
-			
+    def __init__(self):
+        self.cfg = None
+        self.setup_config()
+        self.tcp = Tcp(port=5000, bufferLen=1024, ip="0.0.0.0")
+        self.tcp.bindSocket()
+        self.log = Logger('main')
+        self.log.addHandler(StreamHandler())
 
-		print("Incoming message: "+msg)
-except KeyboardInterrupt:
-	gpio.cleanup()
-	print("Exit")
+    def __call__(self):
+        self.log.info("Server running:")
+        try:
+            while True:
+                self.log.debug('Waiting for msg')
+                msg = self.tcp.recvMsg()
 
-sys.exit()
-    
+                if "Button 1" in msg:
+                    if not isOutputPinHigh(OPINS[0]):
+                        gpio.output(OPINS[0], gpio.HIGH)
+                    elif isOutputPinHigh(OPINS[0]):
+                        gpio.output(OPINS[0], gpio.LOW)
+
+                elif "Button 2" in msg:
+                    gpio.output(OPINS[0], gpio.LOW)
+
+                elif "ALL_OFF" in msg:
+                    for pin in OPINS:
+                        if pin:
+                            if isOutputPinHigh(pin):
+                                gpio.output(pin, gpio.LOW)
+
+                elif "Cron:" in msg:
+                    # Expects to get every config entry in the right order.
+                    newMsg = msg.replace("Cron:", "")
+                    listMsg = newMsg.split("|")
+                    for (value, cfg_entry) in zip(listMsg, self.cfg.cron):
+                        self.cfg.cron[cfg_entry] = value
+                    self.save_cfg()
+
+                self.log.info("Incoming message: %s", msg)
+        except KeyboardInterrupt:
+            self.log.info("Exit")
+
+    def __del__(self):
+        '''Gets always executet if the instance is destroyed.
+        Also if an Exeption happens
+        '''
+        gpio.cleanup()
+
+    def setup_config(self):
+        self.cfg = ConfigParser()
+        self.cfg.read("config.ini")
+        self.cfg.cron = self.cfg['Cron']
+
+    def save_cfg(self):
+        '''saves config'''
+        with open("config.ini", 'w') as configfile:
+            self.cfg.write(configfile)
 
 
+def main():
+    '''Gets only called when run in an python shell, Ignoring imports'''
 
+    # Setup GPIO'S
+    gpio.setwarnings(False)
+    gpio.setmode(gpio.BCM)
+
+    for pin in OPINS:
+        if pin:
+            gpio.setup(pin, gpio.OUT)
+    RpiServer()()
+
+
+if __name__ == '__main__':
+    main()
